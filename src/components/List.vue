c4b824a7cd5c4031265f999404cb6bcec1b9fb99
realvint <victor.malukhin@gmail.com>
2022-08-24 15:56:41 +0300

    Добавил сортировку

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,15 @@
 <template>
-  <table class="table">
-    <Item v-for='item in items' :key='item.id' :item='item' />
+  <select v-model='sortBy' class='form-select form-select-sm mb-3'>
+    <option value='date'>Sort by date</option>
+    <option value='likes'>Sort by like</option>
+  </select>
+  <table class='table'>
+    <Item v-for='item in sorteredItems' :key='item.id' :item='item' />
   </table>
 </template>
 
 <script>
+  import { ref, computed } from 'vue'
   import Item from '@/components/Item.vue'
 
   export default {
@@ -14,6 +19,18 @@
         type: Array,
         required: true
       }
+    },
+    setup({ items }) {
+      const sortBy = ref('date')
+
+      const sorteredItems = computed(() => {
+        return items.sort((a, b) => {
+          if (a[sortBy.value] > b[sortBy.value]) return -1
+          if (a[sortBy.value] < b[sortBy.value]) return 1
+        })
+      })
+      
+      return { sortBy, sorteredItems }
     }
   }
 </script>
